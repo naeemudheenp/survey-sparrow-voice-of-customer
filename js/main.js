5c3ff50e746a8a3be35d2706343574339c449b5d
naeemudheenp-surveysparrow <nayeemudheenaslam3@gmail.com>
2023-02-14 12:49:48 +0530

    4.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 var menuButtonStatus=false;
 var menuButtonVissibility=false;
 
+
+
 var navBarMenu=document.getElementById("menuButton");
 
-footer_setDate();
+
 
 function navBarMenu_onClick(){
+
+    if(!menuButtonStatus){
+        navBarMenu.classList.remove('fa-bars');
+        navBarMenu.classList.add('fa-xmark');
+
+        menuButtonStatus=true;
+    }else{
+        navBarMenu.classList.remove('fa-xmark');
+        navBarMenu.classList.add('fa-bars');
+
+        menuButtonStatus=false;
+
+    }
  
     var sectionLink=document.getElementById("sectionLink");
     sectionLink.classList.toggle("section_navbar_links_toggle");
+
+    var menuButton = document.getElementById("menuButton");
+
+
 
     var sectionLink1=document.getElementById("sectionLink1");
     sectionLink1.classList.toggle("section_navbar_links_toggle");
